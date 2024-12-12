08e2ab1eb134f730f89bf485e435cb8bc2d02449
charles <charles.chen@megaeth.technology>
2024-12-12 15:18:08 +0800

    Merge remote-tracking branch 'origin/release-1.2' into release-1.2

--- examples/ecdsa/src/main.rs
+++ examples/ecdsa/src/main.rs
@@ -18,7 +18,8 @@ use k256::{
     EncodedPoint,
 };
 use rand_core::OsRng;
-use risc0_zkvm::{default_prover, ExecutorEnv, Receipt};
+use risc0_zkvm::{ExecutorEnv, Receipt};
+use risc0_zkvm::host::server::prove::get_local_prover;
 
 /// Given an secp256k1 verifier key (i.e. public key), message and signature,
 /// runs the ECDSA verifier inside the zkVM and returns a receipt, including a
@@ -37,7 +38,7 @@ fn prove_ecdsa_verification(
         .unwrap();
 
     // Obtain the default prover.
-    let prover = default_prover();
+    let prover = get_local_prover().unwrap();
 
     // Produce a receipt by proving the specified ELF binary.
     prover.prove(env, ECDSA_VERIFY_ELF).unwrap().receipt
@@ -45,15 +46,32 @@ fn prove_ecdsa_verification(
 
 fn main() {
     // Generate a random secp256k1 keypair and sign the message.
+    use std::time::Instant;
+
+    let start = Instant::now();
     let signing_key = SigningKey::random(&mut OsRng); // Serialize with `::to_bytes()`
+    let duration = start.elapsed();
+    println!("Time taken to generate signing key: {:?}", duration);
+
     let message = b"This is a message that will be signed, and verified within the zkVM";
+
+    let start = Instant::now();
     let signature: Signature = signing_key.sign(message);
+    let duration = start.elapsed();
+    println!("Time taken to sign message: {:?}", duration);
 
     // Run signature verified in the zkVM guest and get the resulting receipt.
+    let start = Instant::now();
     let receipt = prove_ecdsa_verification(signing_key.verifying_key(), message, &signature);
+    let duration = start.elapsed();
+    println!("Time taken to prove ECDSA verification: {:?}", duration);
 
     // Verify the receipt and then access the journal.
+    let start = Instant::now();
     receipt.verify(ECDSA_VERIFY_ID).unwrap();
+    let duration = start.elapsed();
+    println!("Time taken to verify receipt: {:?}", duration);
+
     let (receipt_verifying_key, receipt_message): (EncodedPoint, Vec<u8>) =
         receipt.journal.decode().unwrap();
 
